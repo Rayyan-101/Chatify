3555d213650151f170061950e42bd5ecdd682a92
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2026-01-24 09:49:18 +0530

    Updated Message.jsx

--- frontend/src/home/Right/Message.jsx
+++ frontend/src/home/Right/Message.jsx
@@ -9,7 +9,11 @@ const Message = ({message}) => {
   const borderClr = itsMe ? "" : "1px solid white";
   const fontClr = itsMe ? "white" : "black";
 
-  
+  const createdAt=new Date(message.createdAt)
+  const formattedTime=createdAt.toLocaleTimeString([],{
+    hour: '2-digit',
+    minute:'2-digit'
+  })
 
 
   return (
